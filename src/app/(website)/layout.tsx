4fd4e9022187d96e12b5f38f64bd58c9648c2fea
Ibeneme Ikenna <91766075+Ibeneme@users.noreply.github.com>
2026-08-18 17:18:36 +0100

    Update layout.tsx

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -3,6 +3,7 @@ import Navbar from '@/components/shared/navbar/navbar'
 import WidgetScript from '@/components/shared/widget/widget-script'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import { Providers } from '../providers'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -24,6 +25,36 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        {/* Meta Pixel Code */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '2007121080691284');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=2007121080691284&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+
         <Providers>
           <WidgetScript />
           <Navbar />
